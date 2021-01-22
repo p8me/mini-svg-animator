75860ddb1aefec63ad3efdb6d26247cbe1a7a032
pooya <pooya@insaneparticles.com>
2021-01-22 11:12:29 -0500

    smoothed interruption of ongoing animation | circle and ellilpse support | clean up

--- morph-svg.js
+++ morph-svg.js
@@ -9,21 +9,46 @@ const loadSvg = async file => {
     }
 }
 
+class TimeManager {
+    constructor() {
+        this.reset();
+    }
+    reset() {
+        this.t = -1;
+        this.lastTime = -1;
+        this.speed = 1;
+    }
+    decelerate(a) {
+        this.speed -= a;
+        if (this.speed < 0) this.speed = 0;
+    }
+    update(t) {
+        if (this.t === -1) this.t = t;
+        else this.t += (t - this.lastTime) * this.speed;
+        this.lastTime = t;
+        return this.t
+    }
+}
+let start = Date.now()
+function consLog(msg) {
+    console.log(Date.now() - start + ": " + msg);
+}
+
 class MorphSvg {
     constructor(container, stateNames, svgFileNames, animatedIds, /* svg ids to be animated | see readme for more details */
-        easing = 'easeInOutQuad', durationOverDecelerationRatio = 100, /* duration / durationOverDecelerationRatio = deceleration | see readme for more details */
+        easing = 'easeInOutQuad', durationTimesDeceleration = 100, /* duration * deceleration  = durationTimesDeceleration | see readme for more details */
         goNextOnProgress = 10) {
 
         this.stateNames = stateNames;
         this.animatedIds = animatedIds;
         this.numAnimatedIds = animatedIds.length;
         this.easing = easing;
-        this.durationOverDecelerationRatio = durationOverDecelerationRatio;
+        this.durationTimesDeceleration = durationTimesDeceleration;
         this.goNextOnProgress = goNextOnProgress;
 
         this.svgs = []
         for (let i = 0; i < stateNames.length; i++) this.svgs.push([]);
-        
+
         this.svgDocs = [];
         for (let stateIdx = 0; stateIdx < stateNames.length; stateIdx++) {
             loadSvg(svgFileNames[stateIdx]).then(svg => {
@@ -62,52 +87,60 @@ class MorphSvg {
                             }
                         }
                     }
+                    else if (type === 'circle' || type === 'ellipse') {
+                        getAnim = (dur) => {
+                            return {
+                                targets: target,
+                                cx: { value: e.getAttribute("cx") },
+                                cy: { value: e.getAttribute("cy") },
+                                duration: dur,
+                                easing: this.easing
+                            }
+                        }
+                    }
                     this.svgs[stateIdx].push({ e: e, anim: getAnim });
                 }
             });
         }
+
+        this.timeManager = new TimeManager();
+        this.queueIsFull = false;
+        this.anims = [];
+        this.queuedAnim = () => { };
+        this.deceleration = 0.02;
     }
 
-    morph(stateName,
-        duration,
-        deceleration // deceleration of previous animation (if still ongoing)
-    ) {
+    morph(stateName, duration) {
         let stateIdx = this.stateNames.indexOf(stateName);
-        for (let eIdx = 0; eIdx < this.numAnimatedIds; eIdx++) {
-            anime(this.svgs[stateIdx][eIdx].anim(duration));
+        this.queuedAnim = function () {
+            this.timeManager.reset();
+            //consLog(stateName);
+            for (let eIdx = 0; eIdx < this.numAnimatedIds; eIdx++) {
+                this.anims[eIdx] = anime(this.svgs[stateIdx][eIdx].anim(duration));
+            }
+            this.queueIsFull = false;
+        }
+        if (!this.anims.length) { this.queuedAnim(); return; }
+        if (!this.queueIsFull) {
+            //consLog("queue " + stateName);
+            this.deceleration = this.durationTimesDeceleration / duration;
+            requestAnimationFrame(this.loop.bind(this));
+            this.queueIsFull = true;
         }
 
     }
 
-}
-
-states = ["normal", "happy", "scared", "upset"];
-stateToSvg = (state) => "svg/snail-" + state + ".opt.svg";
-svgFiles = []
-for (s of states) svgFiles.push(stateToSvg(s));
-
-svgElements = ["shell", "foot", "ltent", "leye", "rtent", "reye"];
-// svgElements = ["shell", "foot", "ltent", "rtent"];
-
-morphSnail = new MorphSvg(document.getElementById('snail'), states, svgFiles, svgElements);
-
-$(document).ready(function () {
-    $(".happy").hover(
-        function () {
-            $("#snail").addClass("happy-snail");
-            morphSnail.morph("happy", 1200);
-        },
-        function () {
-            $("#snail").removeClass("happy-snail");
-            morphSnail.morph("normal", 3000);
-        });
-});
-
-var snail = document.getElementById("snail");
-
-snail.onmouseover = function () {
-    morphSnail.morph("scared", 1000);
-}
-snail.onmouseout = function () {
-    morphSnail.morph("normal", 3000);
+    loop(t) {
+        //consLog(this.timeManager.speed);
+        this.timeManager.decelerate(this.deceleration);
+        var slowedTime = this.timeManager.update(t);
+        this.anims.forEach(an => an.tick(slowedTime));
+        if (50 - Math.abs(this.anims[0].progress - 50) > this.goNextOnProgress && this.timeManager.speed > 0)
+            requestAnimationFrame(this.loop.bind(this));
+        else {
+            //consLog("slowDown finished");
+            this.anims.forEach(an => an.pause());
+            this.queuedAnim();
+        }
+    }
 }
