e1bc91b403c4ae15e09bd41b0abc2a42a45816fb
pooya <pooya@insaneparticles.com>
2021-01-28 16:42:54 -0500

    organized svg dir
    original (nont optimized) SVGs are located in svg/org which will be optimized into the svg dir.

--- optimize_svgs.py
+++ optimize_svgs.py
@@ -1,18 +1,24 @@
 import scour
 import os
-import re
 
 from scour.scour import (make_well_formed, parse_args, scourString, scourXmlFile, start, run,
                          XML_ENTS_ESCAPE_APOS, XML_ENTS_ESCAPE_QUOT)
 from scour.svg_regex import svg_parser
 
-svg_path = 'svg'
+org_svg_path = os.path.join('svg', 'org')
+opt_svg_path = 'svg'
 
-non_optimized_svgs = [os.path.join(svg_path, f) for f in os.listdir(
-    svg_path) if re.match(r'^((?!opt).)*\.svg', f)]
+org_svgs_files = []
+opt_svgs_files = []
 
-for orgSvg in non_optimized_svgs:
-    optSvg = orgSvg[:-3] + 'opt.svg'
+for f in os.listdir(org_svg_path):
+    if f.endswith(".svg"):
+        org_svgs_files.append(os.path.join(org_svg_path, f))
+        opt_svgs_files.append(os.path.join(opt_svg_path, f)[:-3] + 'opt.svg') 
+
+
+for i, orgSvg in enumerate(org_svgs_files):
+    optSvg = opt_svgs_files[i]
     with open(optSvg, 'w') as outWriter:
         doc = scourXmlFile(orgSvg, parse_args(['--set-precision=3', '--remove-metadata', '--enable-viewboxing',
             '--enable-id-stripping', '--protect-ids-noninkscape', '--strip-xml-prolog',
